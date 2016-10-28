29347ba860fa20edf03b395d1aa6d2a89d787872
luke <lukes611@gmail.com>
2016-10-28 18:43:53 +1000

    adding comments

--- examples/shared memory/client.c
+++ examples/shared memory/client.c
@@ -12,27 +12,29 @@ int main(){
 	//create shared memory
 	SharedMemory sm = newSharedMemory(id, amountOfMemory, SMEM_ACCESS);
 
+	//quit if error
 	if(sm.error){
 		puts("error with shared memory");
 		return 1;
 	}
 	
+	//set up pointers to data
 	int * sync = (int*)sm.ptr;
 	int * number = sync + 1;
 
-
+	//ask the user to enter a number to send to the server to factor
 	int num;
 	printf("enter a number: "), scanf("%d", &num); //set num
 
-	while(*sync != 0) ;// wait for sync to be 0
+	while(*sync != 0) /*wait while the server is not read*/;
 
-	*number = num;
-	*sync = 1;
+	*number = num; //write the number to shared memory
+	*sync = 1; //signal the server the number is available
 
-	while(*sync != -1){
-		if(*sync == 2){
-			int fCopy = *number;
-			*sync = 0;
+	while(*sync != -1){ //whilst the server is not finished
+		if(*sync == 2){ //if there is a factor
+			int fCopy = *number; //read it in from shared memory
+			*sync = 0; //signal that we got that factor to the server
 			printf("factor found: %d\n", *number);
 		}
 	}	
--- examples/shared memory/server.c
+++ examples/shared memory/server.c
@@ -15,37 +15,40 @@ int main(){
 	SharedMemory sm = newSharedMemory(id, amountOfMemory, SMEM_CREATE);
 
 	
+	//quit if error
 	if(sm.error){
 		puts("error with shared memory");
 		return 1;
 	}
 	
+	//get some pointers into shared memory
 	int * sync = (int*)sm.ptr;
 	int * number = sync + 1;
 
+	//initialize them
 	*sync = 0;
 	*number = -1;
 
 	puts("ready...");
 
-	while(*sync != 1) ; //wait for sync to be 1
-	int numCopy = *number;
+	while(*sync != 1) ; //wait for client to enter number
+	int numCopy = *number; //get number
 
-	printf("client says: %d\n", numCopy);
+	printf("client says: %d\n", numCopy); //print number
 
-	*sync = 0;
+	*sync = 0; //reset sync
 
 	int i;
-	for(i = 1; i <= numCopy; i++){
-		if(numCopy%i==0)/*if factor*/{
-			while(*sync == 2) ;
-			*number = i;
-			*sync = 2;
+	for(i = 1; i <= numCopy; i++){ //for each number in range 1-N
+		if(numCopy%i==0)/*if factor, give to client*/{
+			while(*sync == 2) /*wait until client reads any old values*/;
+			*number = i; //write number to shared memory
+			*sync = 2; //let the server know there is a factor to read
 		}
 	}
 
-	while(*sync == 2) ;
-	*sync = -1;
+	while(*sync == 2) /*wait until client reads any old values*/;
+	*sync = -1; //signal client to quit
 
 	puts("server shutdown");
 	return 0;
